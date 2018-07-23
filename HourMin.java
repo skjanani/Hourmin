a800517856389addcc906ca85936dfc4e713bfc7
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-23 14:35:15 +0530

    Add files via upload

---
+++ HourMin.java
@@ -0,0 +1,15 @@
+package janani;
+
+public class HourMin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int h=5,h1=6,min=45,min1=50;
+int a,b;
+a=Math.abs(h1-h);
+b=Math.abs(min1-min);
+System.out.println(a+" "+b);
+		
+	}
+
+}
